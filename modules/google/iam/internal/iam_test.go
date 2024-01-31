0bc1fd58900c006ae3928b13ba1cc8dd17a3f91c
Tomasz Maczukin <tomasz@maczukin.pl>
2024-01-31 17:53:56 +0100

    Add google/iam top-level module

---
+++ modules/google/iam/internal/iam_test.go
@@ -0,0 +1,33 @@
+package manager
+
+import (
+	"testing"
+
+	"gitlab.com/gitlab-org/ci-cd/runner-tools/grit/test_tools"
+)
+
+func TestIAM(t *testing.T) {
+	name := test_tools.JobName(t)
+
+	expectedModules := []string{
+		"google_service_account.default",
+	}
+
+	testCases := map[string]struct {
+		moduleVars      map[string]interface{}
+		expectedModules []string
+	}{
+		"create iam": {
+			moduleVars: map[string]interface{}{
+				"name": name,
+			},
+			expectedModules: expectedModules,
+		},
+	}
+
+	for tn, tc := range testCases {
+		t.Run(tn, func(t *testing.T) {
+			test_tools.PlanAndAssert(t, tc.moduleVars, tc.expectedModules)
+		})
+	}
+}
---
+++ modules/google/iam/prod/iam.tf
@@ -0,0 +1,27 @@
+#######################
+# METADATA VALIDATION #
+#######################
+
+module "validate-name" {
+  source = "../../../internal/validation/name"
+  name   = var.metadata.name
+}
+
+module "validate-support" {
+  source   = "../../../internal/validation/support"
+  use_case = "iam"
+  use_case_support = tomap({
+    "iam" = "experimental"
+  })
+  min_support = var.metadata.min_support
+}
+
+###################
+# IAM PROD MODULE #
+###################
+
+module "iam" {
+  source = "../internal"
+
+  name = var.metadata.name
+}

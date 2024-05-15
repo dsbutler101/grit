8305bdf834552b1c805e1b0609fb2e087f2a925a
Vianney Chacon Madriz <vchacon@gitlab.com>
2024-05-15 19:23:38 +0200

    Modified checks to match variable names

--- scenarios/aws/linux/docker-autoscaler-default/checks.tf
+++ scenarios/aws/linux/docker-autoscaler-default/checks.tf
@@ -5,10 +5,10 @@ module "check-concurrent" {
 
 module "check-max-instances" {
   source  = "../../../../modules/internal/validation/fail_validation"
-  message = var.max_instances <= 1000 ? "" : "Fleeting plugin for AWS will not allow to manage more than 1000 instances at once"
+  message = var.autoscaling_policy.scale_max <= 1000 ? "" : "Fleeting plugin for AWS will not allow to manage more than 1000 instances at once"
 }
 
-module "check-concurrent-and-capacity" {
-  source  = "../../../../modules/internal/validation/fail_validation"
-  message = var.max_instances >= var.concurrent / var.capacity_per_instance ? "" : "max_instances needs to be higher to fit the defined number of concurrent with the given capacity_per_instance"
-}
+ module "check-concurrent-and-capacity" {
+   source  = "../../../../modules/internal/validation/fail_validation"
+   message = var.autoscaling_policy.scale_max >= var.concurrent / var.capacity_per_instance ? "" : "max_instances needs to be higher to fit the defined number of concurrent with the given capacity_per_instance"
+ }

9d271051af36f2b1b18b40296ea67436c646954a
Hannes Hörl <hhoerl@gitlab.com>
2024-05-27 09:36:18 +0200

    Simplify operator manifest handling
    We consume the operator manifests directly from the upstream repo now,
    without restructuring it and generating terraform code.
    
    We now
    - read the "raw" file
    - split it on "---"
    - parse as yaml
    - pull out the data as we need
    - pass that on to the `kubectl_manifest` resource

--- modules/k8s/operator/internal/operator.tf
+++ modules/k8s/operator/internal/operator.tf
@@ -1,3 +1,25 @@
+locals {
+  operator_manifests = {
+    # - split the multi-doc yaml
+    # - parse it
+    # - pull out some data (eg. namespace, ...)
+    # - return map of name => parsed yaml
+    for resource in [
+      for yaml in [
+        for doc in split("\n---\n", file("${path.module}/manifests.yaml")) : yamldecode(doc)
+      ] :
+      {
+        apiVersion = yaml.apiVersion
+        kind       = yaml.kind
+        namespace  = lookup(yaml.metadata, "namespace", "_cluster_scoped_")
+        name       = yaml.metadata.name
+        full       = yaml
+      }
+    ] :
+    "${resource.apiVersion}::${resource.kind}::${resource.namespace}::${resource.name}" => resource.full
+  }
+}
+
 resource "kubectl_manifest" "operator_resources" {
   for_each = {
     for n, m in local.operator_manifests : n => yamlencode(m)
--- modules/k8s/operator/internal/updateManifest.sh
+++ modules/k8s/operator/internal/updateManifest.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-#+ This pulls down the upstream gitlab runner operator manifests, massages them
-#+ into a format that we can pass on to terraform, and puts them into a file
-#+ that will be automatically picked up by terraform.
-#+
-#+ Note:
-#+   The output file needs to be named '.tf.json', otherwise terraform will ignore it
-
 set -e
 set -u
 set -o pipefail
@@ -14,30 +7,16 @@ set -o pipefail
 : "${REPO_BASE_URL:=https://gitlab.com/gitlab-org/gl-openshift/gitlab-runner-operator}"
 : "${REPO_PATH:=operator.k8s.yaml}"
 : "${REV:=master}"
-: "${DEST_FILE:=locals_generated.tf.json}"
+: "${DEST_FILE:=manifests.yaml}"
 
 main() {
   local rawURL="${REPO_BASE_URL}/-/raw/${REV}/${REPO_PATH}"
   local dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" &&  pwd )"
   local destFull="${dir}/${DEST_FILE}"
 
-  curl -fLsS "${rawURL}" \
-    | yq -se '
-        {
-          "//": "auto generated, do not edit",
-          "locals": {
-            "operator_manifests": (
-              map(
-                "\(.apiVersion)::\(.kind)::\(.metadata.namespace//"_cluster_scoped_")::\(.metadata.name)" as $name
-                | { $name : . }
-              ) | add
-            )
-          }
-        }
-      ' \
-    > "${dir}/${DEST_FILE}"
+  curl -fLsS "${rawURL}" -o "${destFull}"
 
-  echo >&2 "# Updated '${dir}/${DEST_FILE}' with restructured content from '${rawURL}'"
+  echo >&2 "# Updated '${destFull}' (source: '${rawURL}')"
 }
 
 main "$@"

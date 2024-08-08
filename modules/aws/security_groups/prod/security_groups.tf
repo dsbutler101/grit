683d3462a308cac57656f2ce6c6fc021148e0fbc
Matt Morrison <mmorrison@gitlab.com>
2024-08-08 14:12:38 +0000

    chore: update fleeting sg rules

--- modules/aws/security_groups/prod/security_groups.tf
+++ modules/aws/security_groups/prod/security_groups.tf
@@ -23,8 +23,12 @@ module "validate-support" {
 module "security_groups" {
   source = "../internal"
 
-  name   = var.metadata.name
-  labels = var.metadata.labels
+  name                       = var.metadata.name
+  labels                     = var.metadata.labels
+  fleeting_inbound_sg_rules  = var.fleeting_inbound_sg_rules
+  fleeting_outbound_sg_rules = var.fleeting_outbound_sg_rules
+  manager_inbound_sg_rules   = var.manager_inbound_sg_rules
+  manager_outbound_sg_rules  = var.manager_outbound_sg_rules
 
   vpc_id = var.vpc_id
 }

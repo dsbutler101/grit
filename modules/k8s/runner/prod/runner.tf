69f0bd1cd6bf7032909d44b37aae38f0f0d32543
Romuald Atchadé <ratchade@gitlab.com>
2024-08-23 09:30:52 -0400

    Add k8s/runner/prod module

---
+++ modules/k8s/runner/prod/runner.tf
@@ -0,0 +1,32 @@
+#######################
+# METADATA VALIDATION #
+#######################
+
+module "validate-support" {
+  source   = "../../../internal/validation/support"
+  use_case = "k8s-operator-runner"
+  use_case_support = tomap({
+    "k8s-operator-runner" = "experimental"
+  })
+  min_support = var.metadata.min_support
+}
+
+##########################
+# K8S RUNNER PROD MODULE #
+##########################
+
+module "runner" {
+  source = "../internal/"
+
+  url             = var.gitlab.url
+  token           = var.gitlab.runner_token
+  namespace       = var.namespace
+  name            = coalesce(var.name_override, var.metadata.name)
+  concurrent      = var.concurrent
+  check_interval  = var.check_interval
+  locked          = var.locked
+  protected       = var.protected
+  run_untagged    = var.run_untagged
+  runner_tags     = var.runner_tags
+  config_template = var.config_template
+}
---
+++ modules/k8s/runner/prod/variables.tf
@@ -0,0 +1,90 @@
+############
+# METADATA #
+############
+
+variable "metadata" {
+  type = object({
+
+    # Unique name used for identification and partitioning resources
+    name = string
+
+    # Labels to apply to supported resources
+    labels = map(string)
+
+    # Minimum required feature support. See https://docs.gitlab.com/ee/policy/experiment-beta-support.html
+    min_support = string
+  })
+}
+
+############################
+# K8S RUNNER CONFIGURATION #
+############################
+
+variable "runner_description" {
+  description = "The description for the GitLab runner"
+  type        = string
+  default     = "default GitLab Runner"
+}
+
+variable "gitlab" {
+  description = "Outputs from the gitlab module. Or your own"
+  type = object({
+    runner_token = string
+    url          = string
+  })
+}
+
+variable "name_override" {
+  type    = string
+  default = null
+}
+
+variable "namespace" {
+  type = string
+}
+
+################################
+# RUNNER MANAGER CONFIGURATION #
+################################
+
+variable "concurrent" {
+  type        = number
+  description = "Number of maximum concurrent jobs handled by Runner at once"
+  default     = 5
+}
+
+variable "check_interval" {
+  type        = number
+  description = "Number of seconds between subsequent requests checking if GitLab has a new job for the Runner"
+  default     = 3
+}
+
+variable "locked" {
+  type        = bool
+  description = "Specify whether the runner should be locked to a specific project"
+  default     = false
+}
+
+variable "protected" {
+  type        = bool
+  description = "Specify whether the runner should only run protected branches"
+  default     = false
+}
+
+variable "runner_tags" {
+  type        = list(string)
+  description = "Specify if jobs without tags should be run. When no runner_tags are set, it will always be true, else it will default to false"
+  default     = []
+}
+
+variable "run_untagged" {
+  type        = bool
+  description = "List of comma separated tags to be applied to the runner"
+  default     = false
+}
+
+variable "config_template" {
+  type        = string
+  description = "A config.toml template provided to configure the runner"
+  default     = ""
+}

782b1b9b8273c91899cb31630cdc6c7c2fd3c515
Romuald Atchadé <ratchade@gitlab.com>
2024-08-23 09:31:06 -0400

    Adjust k8s/runner/test module

--- modules/k8s/runner/test/runner.tf
+++ modules/k8s/runner/test/runner.tf
@@ -5,5 +5,11 @@ module "runner" {
   token           = var.gitlab.runner_token
   namespace       = var.namespace
   name            = coalesce(var.name_override, var.metadata.name)
+  concurrent      = var.concurrent
+  check_interval  = var.check_interval
+  locked          = var.locked
+  protected       = var.protected
+  run_untagged    = var.run_untagged
+  runner_tags     = var.runner_tags
   config_template = var.config_template
 }
--- modules/k8s/runner/test/variables.tf
+++ modules/k8s/runner/test/variables.tf
@@ -26,6 +26,48 @@ variable "namespace" {
   type = string
 }
 
+################################
+# RUNNER MANAGER CONFIGURATION #
+################################
+
+variable "concurrent" {
+  type        = number
+  description = "Number of maximum concurrent jobs handled by Runner at once"
+  default     = 5
+}
+
+variable "check_interval" {
+  type        = number
+  description = "Number of seconds between subsequent requests checking if GitLab has a new job for the Runner"
+  default     = 3
+}
+
+variable "locked" {
+  type        = bool
+  description = "Specify whether the runner should be locked to a specific project"
+  default     = false
+}
+
+variable "protected" {
+  type        = bool
+  description = "Specify whether the runner should only run protected branches"
+  default     = false
+}
+
+variable "runner_tags" {
+  type        = list(string)
+  description = "Specify if jobs without tags should be run. When no runner_tags are set, it will always be true, else it will default to false"
+  default     = []
+}
+
+variable "run_untagged" {
+  type        = bool
+  description = "List of comma separated tags to be applied to the runner"
+  default     = false
+}
+
 variable "config_template" {
-  type = string
+  type        = string
+  description = "A config.toml template provided to configure the runner"
+  default     = ""
 }

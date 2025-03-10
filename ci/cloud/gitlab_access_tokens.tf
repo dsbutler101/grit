d6e81330534ecab3b88fe2d068f10a145fb1b9d3
Joe Shaw <jshaw@gitlab.com>
2025-03-10 17:05:19 +0000

    ci: remove unused grit-e2e tokens and variables

--- ci/cloud/gitlab_access_tokens.tf
+++ ci/cloud/gitlab_access_tokens.tf
@@ -1,16 +1,3 @@
-resource "gitlab_project_access_token" "e2e-tests-trigger" {
-  project      = data.gitlab_project.grit-e2e.id
-  name         = "e2e-tests-trigger"
-  access_level = "maintainer"
-
-  scopes = ["api"]
-
-  rotation_configuration = {
-    expiration_days    = 365
-    rotate_before_days = 30
-  }
-}
-
 resource "gitlab_project_access_token" "gandalf-security-scanning-tool" {
   project      = data.gitlab_project.grit.id
   name         = "gandalf-security-scanning-tool"
--- ci/cloud/gitlab_projects.tf
+++ ci/cloud/gitlab_projects.tf
@@ -1,7 +1,3 @@
 data "gitlab_project" "grit" {
   path_with_namespace = "gitlab-org/ci-cd/runner-tools/grit"
 }
-
-data "gitlab_project" "grit-e2e" {
-  path_with_namespace = "gitlab-org/ci-cd/runner-tools/grit-e2e"
-}

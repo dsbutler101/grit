125b6aae01a925712ec9343a57dbbfeac2b7e777
Tomasz Maczukin <tomasz@maczukin.pl>
2025-04-11 14:35:07 +0200

    Improve e2e:state-clean job
    This commit brings two changes:
    
    1. Moves e2e:state-clean script definition to a dedicated script file.
       This makes both the YAML definition simpler and the script itself
       easier to maintain.
    
    2. Updates the script so that it doesn't fail on the first locked state
       that is applicable for cleanup.
    
    There may be cases where a state will be left in a locked state, like
    when a failure happened in the middle of Terraform execution. Such state
    can't be deleted until the existing lock is removed.
    
    Current implementtion of `e2e:state-clean` would try to remove it anyway,
    failing and therefore exiting the job before it would even approach to
    remove the other states. If the first listed and applicable for deletion
    state is locked, `e2e:state-clean` would keep failing for all pipelines.
    
    This commit excludes such states from the removal loop, so that we don't
    fail the job and we try to remove as much of stale states as possible in
    each pipeline.
    
    States older than 24 hours that are currently locked most probably need
    a manual removal anyway. Therefore `e2e:state-clean` will now list them,
    so we know what to look for.

---
+++ ci/e2e-state-clean.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# graphql query
+query="{
+  project(fullPath: \"gitlab-org/ci-cd/runner-tools/grit\") {
+    terraformStates(first: 100) {
+      nodes {
+        name
+        createdAt
+        lockedAt
+      }
+    }
+  }
+}"
+
+# shellcheck disable=SC2016
+data="$(gojq -nc --arg q "${query}" '{"query": $q}')"
+
+resp="$(curl -s "https://gitlab.com/api/graphql?private_token=${GITLAB_TOKEN_TERRAFORM}" -H "Content-Type: application/json" -X POST -d "${data}")"
+
+# filter out non-e2e states and states those created in last 24 hours that are not locked
+states="$(echo "${resp}" | gojq -r '.data.project.terraformStates.nodes[] | select((.name | test("^u[0-9]")) and (now - (.createdAt | fromdate) > 86400) and (.lockedAt == null)) | .name')"
+
+# destroy
+for state in ${states}; do
+  echo -e "\033[31;1mDestroying terraform state \033[37;1m${state}\033[0m"
+  export TF_STATE_NAME="${state}"
+  ./scripts/e2e-destroy.sh
+done
+
+states="$(echo "${resp}" | gojq -r '.data.project.terraformStates.nodes[] | select((.name | test("^u[0-9]")) and (now - (.createdAt | fromdate) > 86400) and (.lockedAt != null)) | .name')"
+if [[ -n "${states}" ]]; then
+  echo -e "\n---\n"
+  echo "Following E2E TF states are older than 24 hours and are currently locked"
+  echo -e "This most probably means they will require manual cleanup\n"
+
+  for state in ${states}; do
+    echo "- ${state}"
+  done
+fi

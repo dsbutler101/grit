6c612e7e61cf7606118ac73bca584954baed8167
Daniel Butler <dsbutler@google.com>
2025-06-18 16:09:32 +0100

    disabled oslogin for ephemeral runners as this prevents runner-manager from being able to dispatch jobs

--- modules/google/fleeting/gce/main.tf
+++ modules/google/fleeting/gce/main.tf
@@ -17,7 +17,7 @@ resource "google_compute_instance_template" "ephemeral_runner" {
   machine_type = var.machine_type
 
   metadata = {
-    enable-oslogin      = true
+    enable-oslogin      = false
     cos-update-strategy = "update_disabled"
   }
 
